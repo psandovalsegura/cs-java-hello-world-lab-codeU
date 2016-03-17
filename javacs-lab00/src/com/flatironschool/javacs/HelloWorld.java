f4b81e248a86edb4fad3849ea4b3638b7732df99
Pedro Sandoval <psandoval2015@gmail.com>
2016-03-16 23:21:26 -0700

    Complete

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,7 +2,7 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+        return Double.parseDouble(System.getProperty("java.specification.version"));
     }
 
     public static void main(String[] args) {
